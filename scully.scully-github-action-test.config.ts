c1e264cafd8e45ccc4b12a1157ea55e8bd6657cf
Chris Griffing <cmgriffing@gmail.com>
2021-05-20 13:43:51 -0700

    feat: scaffold project to scully basics

---
+++ scully.scully-github-action-test.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "scully-github-action-test",
+  outDir: './dist/static',
+  routes: {
+  }
+};
